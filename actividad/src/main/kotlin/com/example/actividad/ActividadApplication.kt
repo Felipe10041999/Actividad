83fb4f7ae165a3421f2e57cabb4080b0dcd30cee
Felipe10041999 <felipemosquera364@gmail.com>
2025-09-03 08:16:30 -0500

    Docs: Se sube proyecto spring boot con conexion a mysql y dependecia jdbc ya configurada

---
+++ actividad/src/main/kotlin/com/example/actividad/ActividadApplication.kt
@@ -0,0 +1,11 @@
+package com.example.actividad
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActividadApplication
+
+fun main(args: Array<String>) {
+	runApplication<ActividadApplication>(*args)
+}
